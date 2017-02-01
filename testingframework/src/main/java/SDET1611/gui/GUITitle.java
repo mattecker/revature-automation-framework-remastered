8b3fe7379dedb2edccd3fed1238005abbe92d7e6
NilimaManandhar <nilimamanandh@gmail.com>
2017-02-01 10:00:41 -0500

    Add files via upload

--- testingframework/src/main/java/SDET1611/gui/GUITitle.java
+++ testingframework/src/main/java/SDET1611/gui/GUITitle.java
@@ -1,10 +1,17 @@
 package SDET1611.gui;
 
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class GUITitle extends JPanel {
 	public GUITitle()
 	{
 		JLabel TestTitle= new JLabel("Hybrid Testing App");
+		TestTitle.setFont(new Font("Serif", Font.BOLD, 35));
+		TestTitle.setForeground(Color.GRAY);
 		add(TestTitle);
 	}
 }
